2a25fc003a0e7ce7fcf7e28c497da8e88d41be1f
Matthew Huff <matthewhuff89@gmail.com>
2025-09-29 15:07:52 -0400

    create db migrations and schema

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,8 +2,8 @@ import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Dream Machine" },
+    { name: "description", content: "Welcome to the Dream Machine." },
   ];
 }
 
---
+++ db/db.ts
@@ -0,0 +1,5 @@
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from 'postgres'
+
+console.log(process.env.DATABASE_URL)
+export const db = drizzle(postgres(process.env.DATABASE_URL!))

a83334b61e82c388cdc4b5774d3895b8e691998f
Matthew Huff <matthewhuff89@gmail.com>
2025-10-01 15:03:30 -0400

    background color of components changed to gold-yellow

--- app/components/signup.tsx
+++ app/components/signup.tsx
@@ -31,14 +31,14 @@ export default function SignUp() {
   
   return (
       <div className="flex justify-center gap-10">
-        <Form className="flex flex-col gap-1 p-8 rounded-lg bg-blue-700 text-white border-blue-700" onSubmit={signUp} method="post">
+        <Form className="flex flex-col gap-1 p-8 rounded-lg bg-yellow-500 text-white border-blue-700" onSubmit={signUp} method="post">
           <label className="font-bold" htmlFor="name">Name</label>
           <input onChange={(e) => setName(e.target.value)} className="rounded-sm bg-white text-black" name="name" type="text" />
           <label className="font-bold" htmlFor="email">Email</label>
           <input onChange={(e) => setEmail(e.target.value)} className="rounded-sm bg-white text-black" name="email" type="text" />
           <label className="font-bold" htmlFor="password">Password</label>
-          <input onChange={(e) => setPassword(e.target.value)} className="rounded-sm bg-white text-black"name="password" type="password" />
-          <button className='bg-blue-900 rounded-lg p-2' type="submit">Sign Up</button>
+          <input onChange={(e) => setPassword(e.target.value)} className="rounded-sm mb-2 bg-white text-black"name="password" type="password" />
+          <button className='bg-blue-500 rounded-lg p-2' type="submit">Sign Up</button>
         </Form>
       </div>
   )

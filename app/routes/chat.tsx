cd96eee3f1574e75b3f0c50b8be07f3040f1a577
Matthew Huff <matthewhuff89@gmail.com>
2025-10-01 11:40:42 -0400

    mobile and responsive styling

--- app/routes/chat.tsx
+++ app/routes/chat.tsx
@@ -14,9 +14,9 @@ export default function Chat() {
   })})
 
   return (
-    <div className='flex flex-col justify-center gap-8'>
-      <h1 className='text-white font-[Faculty_Glyphic] font-extrabold text-center mt-12 text-6xl'>CLEARCHAT</h1>
-      <div className="flex flex-col font-[Faculty_Glyphic] gap-4 rounded-lg p-8 h-[45rem] w-[50rem] max-w-5xl py-24 mx-auto overflow-y-auto bg-yellow-500">
+    <div className='flex flex-col justify-center m-6 gap-8'>
+      <h1 className='text-white font-[Faculty_Glyphic] font-extrabold my-9 text-center text-5xl md:text-6xl'>CLEARCHAT</h1>
+      <div className="flex flex-col justify-center font-[Faculty_Glyphic] gap-4 rounded-lg p-10 h-130 md:h-[40rem] w-full md:w-[50rem]  max-w-5xl py-24 mx-auto bg-yellow-500">
         <ScrollArea className='h-full'>
           {messages.map(message => (
           <div key={message.id} className="bg-yellow-500 font-bold text-white whitespace-pre-wrap">
@@ -24,7 +24,7 @@ export default function Chat() {
             {message.parts.map((part, i) => {
               switch (part.type) {
                 case 'text':
-                  return <div className="bg-yellow-500 text-xl font-light text-white" key={`${message.id}-${i}`}>{part.text}</div>
+                  return <div className="bg-yellow-500 text-md md:text-xl font-light text-white" key={`${message.id}-${i}`}>{part.text}</div>
                 case 'tool-weather':
                   return (
                     <pre key={`${message.id}-${i}`}>
@@ -37,7 +37,7 @@ export default function Chat() {
         ))}
         </ScrollArea>
         <Form
-          className='flex w-full gap-5 justify-center self-center'
+          className='flex items-center mt-4 w-full gap-5 justify-center'
           onSubmit={e => {
             e.preventDefault()
             sendMessage({ text: input })
@@ -45,12 +45,12 @@ export default function Chat() {
           }}
         >
           <input 
-            className='bg-white dark:bg-white w-full max-w-md p-2 mt-12 border border-zinc-300 rounded shadow-xl'
+            className='bg-white dark:bg-white w-full max-w-md p-2 border border-zinc-300 rounded shadow-xl'
             value={input}
             placeholder='Say something...'
             onChange={e => setInput(e.currentTarget.value)}
           />
-          <Button className='bg-blue-400 self-center mt-12'>Chat!</Button>
+          <Button className='bg-blue-400 self-center'>Chat!</Button>
         </Form>
       </div>
     </div>

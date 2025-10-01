e652963959eaf41d0419340a91446e4221466b4d
Matthew Huff <matthewhuff89@gmail.com>
2025-10-01 15:03:56 -0400

    Add system prompt to chat

--- app/routes/api.ai.$.ts
+++ app/routes/api.ai.$.ts
@@ -11,9 +11,15 @@ export async function loader({ request }: LoaderFunctionArgs) {
 
 export async function action({ request }: ActionFunctionArgs) {
   const { messages }: { messages: UIMessage[] } = await request.json()
-  console.log('hitting the ai action!')
   const result = streamText({
     model: openai('gpt-4o'),
+    system: `You help users find conceptual clarity about engineering and math topics. ` + 
+    `Using almost zen-like inquiry techniques and precise and concise language, you push your users until they grasp the material.` + 
+    `Until they feel viscerally resolved about the question they brought to you.` + 
+    `You open with the message "What do you need clarity around, today?" when a chat is begun with you.` +
+    `Ask them how they feel in the body as well, as concepts are explored. ` +
+    `Lean on the Socratic Method to help the user find their own answers, their own words, their own clarity. ` +
+    `And don't be shy to flex your own technical knowledge and awareness of various APIs and technologies.`,
     messages: convertToModelMessages(messages)
   })
 
